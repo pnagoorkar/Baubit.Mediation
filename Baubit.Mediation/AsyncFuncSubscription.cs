273b2e8ae4b84454fcd15d2ecd5f9e7b6b5cd319
pnagoorkar <prashant.nagoorkar@gmail.com>
2026-01-11 15:40:02 -0500

    Solidification.

---
+++ Baubit.Mediation/AsyncFuncSubscription.cs
@@ -0,0 +1,37 @@
+﻿using Baubit.Caching;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Baubit.Mediation
+{
+    internal class AsyncFuncSubscription<TRequest, TResponse> : Subscription<TRequest, TResponse> where TRequest : IRequest<TResponse> where TResponse : IResponse
+    {
+        public Func<TRequest, CancellationToken, Task<TResponse>> FuncHandler { get; private set; }
+
+        internal AsyncFuncSubscription(Func<TRequest, CancellationToken, Task<TResponse>> funcHandler,
+                                       bool enableBuffering) : base(enableBuffering)
+        {
+            FuncHandler = funcHandler;
+        }
+
+        protected override async Task<bool> ProcessBufferAsync(IOrderedCache<long, object> cache, string name, CancellationToken cancellationToken = default)
+        {
+            await using var enumerator = cache.GetFutureAsyncEnumerator(name, cancellationToken);
+            while (await enumerator.MoveNextAsync())
+            {
+                if (enumerator.Current.Value is TrackedRequest<TRequest, TResponse> trackedRequest)
+                {
+                    var response = await FuncHandler.Invoke(trackedRequest.Request, cancellationToken);
+                    cache.Add(new TrackedResponse<TResponse>(trackedRequest.Id, response), out _);
+                }
+            }
+            return true;
+        }
+
+        protected override async Task<TResponse> DispatchAsync(TRequest request, CancellationToken cancellationToken = default)
+        {
+            return await FuncHandler.Invoke(request, cancellationToken);
+        }
+    }
+}
---
+++ Baubit.Mediation/SyncInterfaceSubscription.cs
@@ -0,0 +1,36 @@
+﻿using Baubit.Caching;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Baubit.Mediation
+{
+    internal class SyncInterfaceSubscription<TRequest, TResponse> : Subscription<TRequest, TResponse> where TRequest : IRequest<TResponse> where TResponse : IResponse
+    {
+        public IRequestHandler<TRequest, TResponse> SyncHandler { get; private set; }
+
+        internal SyncInterfaceSubscription(IRequestHandler<TRequest, TResponse> syncHandler,
+                                     bool enableBuffering) : base(enableBuffering)
+        {
+            SyncHandler = syncHandler;
+        }
+
+        protected override async Task<bool> ProcessBufferAsync(IOrderedCache<long, object> cache, string name, CancellationToken cancellationToken = default)
+        {
+            await using var enumerator = cache.GetFutureAsyncEnumerator(name, cancellationToken);
+            while (await enumerator.MoveNextAsync())
+            {
+                if (enumerator.Current.Value is TrackedRequest<TRequest, TResponse> trackedRequest)
+                {
+                    var response = SyncHandler.Handle(trackedRequest.Request);
+                    cache.Add(new TrackedResponse<TResponse>(trackedRequest.Id, response), out _);
+                }
+            }
+            return true;
+        }
+
+        protected override Task<TResponse> DispatchAsync(TRequest request, CancellationToken cancellationToken = default)
+        {
+            return Task.FromResult(SyncHandler.Handle(request));
+        }
+    }
+}
